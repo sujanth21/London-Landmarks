1d1e8b90c40335cacdd5efdb7fa3c7c737f8ae1e
Sujanth <sujanth1987@gmail.com>
2017-08-27 17:35:58 +1000

    Cell UI created

---
+++ London Landmarks/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  London Landmarks
+//
+//  Created by Sujanth Sebamalaithasan on 27/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
